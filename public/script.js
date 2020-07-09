8733741966eccc6e5eb9cb7321ca2c50de1f8489
Glitch (hello-express) <none>
2020-07-09 05:10:26 +0000

    📫👁 Checkpoint ./public/style.css:340482/462 ./public/script.js:340482/303

--- public/script.js
+++ public/script.js
@@ -2,17 +2,21 @@ const dnsEntryList = document.getElementById("results");
 const form = document.querySelector("form");
 
 form.addEventListener("submit", event => {
+  const url = form.elements.url.value
   event.preventDefault();
+  dnsEntryList.innerHTML = '<div class="loader" />';
   apiCall("/dns", {
-    hostname: new URL(form.elements.url.value).hostname
+    hostname: new URL(url).hostname
   })
     .then(response => response.json())
     .then(response => {
+      dnsEntryList.innerHTML = '';
       if (response.results) {
         response.results.forEach(entry => {
           const newListItem = document.createElement("li");
           newListItem.innerText = entry.type + " " + entry.address;
           dnsEntryList.appendChild(newListItem);
+          apiCall("/request", {url: url, ip: entry.address})
         });
         console.log(response.results);
       } else if (response.error) {
