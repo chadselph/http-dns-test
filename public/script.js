81924e425cdbf90a75a77d8c840c412e7860200a
Glitch (hello-express) <none>
2020-07-09 04:47:24 +0000

    🐀🔔 Checkpoint ./public/script.js:340482/749

--- public/script.js
+++ public/script.js
@@ -1,20 +1,40 @@
 // client-side js, loaded by index.html
 // run by the browser each time the page is loaded
 
-console.log("hello world :o");
-
 // define variables that reference elements on our page
-const dreamsList = document.getElementById("dreams");
-const dreamsForm = document.querySelector("form");
+const dnsEntryList = document.getElementById("results");
+const form = document.querySelector("form");
 
 // a helper function that creates a list item for a given dream
 function appendNewDream(dream) {
   const newListItem = document.createElement("li");
   newListItem.innerText = dream;
-  dreamsList.appendChild(newListItem);
+  dnsEntryList.appendChild(newListItem);
 }
 
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  fetch("/dns", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      "hostname": new URL(form.elements.url.value).hostname
+    })
+  })
+    .then(response => response.json())
+  .then(response => {
+    if (response.results) {
+      console.log(response.results);
+    } else if (response.error) {
+      console.log(response.error);
+    }
+  })
+})
+
 // fetch the initial list of dreams
+/*
 fetch("/dreams")
   .then(response => response.json()) // parse the JSON from the server
   .then(dreams => {
@@ -39,3 +59,4 @@ fetch("/dreams")
       dreamsForm.elements.dream.focus();
     });
   });
+  */
