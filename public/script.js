178111660900404ae5d7fddf7fc4325ff547acdb
Glitch (hello-express) <none>
2020-07-09 04:58:24 +0000

    💝🌄 Checkpoint ./public/script.js:340482/7632 ./views/index.html:340482/53

--- public/script.js
+++ public/script.js
@@ -1,62 +1,32 @@
-// client-side js, loaded by index.html
-// run by the browser each time the page is loaded
-
-// define variables that reference elements on our page
 const dnsEntryList = document.getElementById("results");
 const form = document.querySelector("form");
 
-// a helper function that creates a list item for a given dream
-function appendNewDream(dream) {
-  const newListItem = document.createElement("li");
-  newListItem.innerText = dream;
-  dnsEntryList.appendChild(newListItem);
-}
-
 form.addEventListener("submit", event => {
   event.preventDefault();
-  fetch("/dns", {
+  apiCall("/dns", {
+    hostname: new URL(form.elements.url.value).hostname
+  })
+    .then(response => response.json())
+    .then(response => {
+      if (response.results) {
+        response.results.forEach(entry => {
+          const newListItem = document.createElement("li");
+          newListItem.innerText = entry.type + " " + entry.address;
+          dnsEntryList.appendChild(newListItem);
+        });
+        console.log(response.results);
+      } else if (response.error) {
+        console.log(response.error);
+      }
+    });
+});
+
+function apiCall(url, body) {
+  return fetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify({
-      "hostname": new URL(form.elements.url.value).hostname
-    })
-  })
-    .then(response => response.json())
-  .then(response => {
-    if (response.results) {
-      console.log(response.results);
-    } else if (response.error) {
-      console.log(response.error);
-    }
-  })
-})
-
-// fetch the initial list of dreams
-/*
-fetch("/dreams")
-  .then(response => response.json()) // parse the JSON from the server
-  .then(dreams => {
-    // remove the loading text
-    dreamsList.firstElementChild.remove();
-  
-    // iterate through every dream and add it to our page
-    dreams.forEach(appendNewDream);
-  
-    // listen for the form to be submitted and add a new dream when it is
-    dreamsForm.addEventListener("submit", event => {
-      // stop our form submission from refreshing the page
-      event.preventDefault();
-
-      // get dream value and add it to the list
-      let newDream = dreamsForm.elements.dream.value;
-      dreams.push(newDream);
-      appendNewDream(newDream);
-
-      // reset form
-      dreamsForm.reset();
-      dreamsForm.elements.dream.focus();
-    });
+    body: JSON.stringify(body)
   });
-  */
+}
